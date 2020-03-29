4ce4dabc33fbc7f8761a5767c2224a1db411c418
Eunsol Lee <eunsol@medibloc.org>
2020-03-29 20:05:51 +0900

    solution 3 added

---
+++ 2019 QR 3 Cryptopanagrams 2.py
@@ -0,0 +1,44 @@
+from math import sqrt
+
+T = int(input())
+
+
+def GCD(a, b):
+    if b == 0:
+        return a
+    return b, a % b
+
+
+def findValue(n):
+    if n == L:
+        return
+    if numbers[n] == 0:
+        if multiple[n] == multiple[n + 1]:
+            findValue(n + 1)
+            numbers[n] = multiple[n] // numbers[n + 1]
+        else:
+            numbers[n + 1] = GCD(multiple[n], multiple[n + 1])
+            numbers[n] = multiple[n] // numbers[n + 1]
+            findValue(n + 1)
+    else:
+        numbers[n + 1] = multiple[n] // numbers[n]
+        findValue(n + 1)
+
+
+for t in range(1, T + 1):
+    N, L = map(int, input().split())
+    multiple = list(map(int, input().split()))
+    numbers = [0] * (L + 1)
+    maps = {}
+    result = ''
+
+    findValue(0)
+
+    uniqueNumbers = list(set(numbers))
+    uniqueNumbers.sort()
+    for i in range(len(uniqueNumbers)):
+        maps[uniqueNumbers[i]] = chr(65 + i)
+    for x in numbers:
+        result += maps[x]
+
+    print('Case #{}: {}'.format(t, result))
